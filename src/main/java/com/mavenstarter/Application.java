af04793be4c7320c0de83054f8723d8ca49d94fc
Greg Yotz <greg@yotzee.com>
2016-06-11 17:40:03 -0500

    init maven starter

---
+++ src/main/java/com/mavenstarter/Application.java
@@ -0,0 +1,18 @@
+package com.mavenstarter;
+
+/**
+ * Created by gregyotz on 6/9/16.
+ */
+public class Application {
+
+
+    public static void main(String args[]) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+
+
+
+        System.out.print("testing application");
+    }
+}
